74fc3c0e6f6de3c668e240d36a53ecbb669bab90
junghwa <junghwa1996@naver.com>
2021-05-01 22:40:46 +0900

    df

---
+++ js/common.js
@@ -0,0 +1,12 @@
+var start = document.getElementById('start_btn');
+var info = document.getElementById('info-box').className;
+
+
+var infoClass = info.add(active);
+console.log(infoClass);
+
+// function infoOpen() {
+//    info.classList.add('active');
+// }
+
+// start.addEventListener('click',infoOpen);
