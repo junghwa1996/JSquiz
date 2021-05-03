ccaae26c919c1adf160b322a03cd008b6ce72136
junghwa <junghwa1996@naver.com>
2021-05-03 17:44:27 +0900

    class 추가

--- js/common.js
+++ js/common.js
@@ -3,11 +3,11 @@ var infoBox = $('.info-box');
 
 start.click(function () {
    infoBox.addClass('active');
+   var falseBtn = $('.false');
+   // var trueBtn = $('.true');
+
+   falseBtn.click(function () {
+      infoBox.removeClass('active');
+   });
 });
 
-var falseBtn = $('.false');
-var trueBtn = $('.true');
-
-falseBtn.click(function () {
-   infoBox.removeClass('active');
-});
